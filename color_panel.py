ad2e1cc6360b30bc9478b768356ff0c4659c4051
Matt Christie <christiemj09@gmail.com>
2018-04-14 14:53:29 -0500

    Experimenting with panels of color.

---
+++ color_panel.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+Make color panels.
+"""
+
+import sys
+
+from PIL import Image
+                
---
+++ pil_experiments.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""
+Experiment with PIL.
+"""
+
+import sys
+
+from PIL import Image
